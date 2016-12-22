935fa8ec2967d9e617264e3e293ccaf8bd9fa91e
Markus Vock <markus.vock@hornbach.de>
2016-12-22 13:26:30 +0100

    Make all variables local

--- jwt.plugin.zsh
+++ jwt.plugin.zsh
@@ -1,4 +1,5 @@
-usage="jwt: Create a JSON Web Token
+function jwt {
+    local usage="jwt: Create a JSON Web Token
 
 Syntax: jwt <payload> <key>
 
@@ -6,21 +7,20 @@ Payload must be JSON-encoded. Key must be the plaintext encryption key.
 
 Example: jwt '"'{"name":"John Doe","role":"Global Administrator"}'"' \"\$uper-secret\""
 
-function jwt {
-    alg="HS256"
-    typ="JWT"
-    header="{\"alg\":\"$alg\",\"typ\":\"$typ\"}"
+    local alg="HS256"
+    local typ="JWT"
+    local header="{\"alg\":\"$alg\",\"typ\":\"$typ\"}"
 
     if [[ $# < 2 || -n "${*[(r)--help]}" ]]; then
         echo "$usage"
         return 1
     fi
 
-    payload="$1"
-    key="$2"
+    local payload="$1"
+    local key="$2"
     shift 2
 
-    claim=$( echo -n "$header" | base64 ).$( echo -n "$payload" | base64 )
+    local claim=$( echo -n "$header" | base64 ).$( echo -n "$payload" | base64 )
 
     echo "$claim".$( echo -n "$claim" | openssl sha256 -hmac "$key" -binary | base64 )
 }
